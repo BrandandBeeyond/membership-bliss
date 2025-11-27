711493e167150715c5504485e17f67270f5a8252
dhiraj <dhirajmymodak22@gmail.com>
2025-11-27 18:34:55 +0530

    some fixes

--- src/config/Key.js
+++ src/config/Key.js
@@ -1,5 +1,5 @@
 export const WEB_API_KEY =
-  '52578771094-lqhn84cjsshp8imd1p2tr6aev7s75g4b.apps.googleusercontent.com';
+  '525787711094-lqhn84cjsshp8imd1p2tr6aev7s75g4b.apps.googleusercontent.com';
 export const ANDROID_API_KEY =
-  '52578771194-aakb9pmvt9uh5ql4g27da9cta1r0s7p1.apps.googleusercontent.com';
-export const API_SERVER = 'https://membership-bliss-server.onrender.com/api/v1';
+  '525787711094-aakb9pmvt9uh5ql4g27da9cta1r0s7p1.apps.googleusercontent.com';
+export const API_SERVER = 'https://membership-bliss-server.onrender.com/api/v1'
